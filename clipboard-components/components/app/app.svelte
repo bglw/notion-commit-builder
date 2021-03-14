24fd7292762c21691fed897fd7a9f909767154b3
Liam Bigelow <liam@cloudcannon.com>
2021-03-15 12:25:20 +1300

    Helpful error handling passed through sometimes :)

--- clipboard-components/components/app/app.svelte
+++ clipboard-components/components/app/app.svelte
@@ -5,22 +5,31 @@
 	import processHashData from './bits/processData';
 
 	let commitData = [];
+	let ogAuthor = '';
 	let author = '';
 	let authors = [];
 	let app = '';
 	let apps = [];
+	let error = '';
 
 	let failed = false;
 	let copied = false;
 
 	onMount(() => {
 		let u = new URL(window.location);
-		let data = decodeURIComponent(u.hash.substr(1)).replace(/\n/g, '');
-		({ commitData, author, authors, apps } = processHashData(data));
+		let data = decodeURIComponent(u.hash.substr(1));
 
+		if (/^error--/.test(data)) {
+			error = data.replace(/^error--/, '');
+		}
+
+		({ commitData, author, authors, apps } = processHashData(data.replace(/\n/g, '')));
+		ogAuthor = author;
 
 		window.addEventListener('copy', (event) => {
 			event.preventDefault();
+
+			if (!commitData) return;
 
 			const blocks = buildNotionBlocks(commitData);
 			if (!blocks) {
@@ -47,72 +56,93 @@
 	}
 
 	$: {
-		for (let cd of commitData) {
-			cd.shown = false;
-			for (let c of cd.commits) {
-				if (c.selected) cd.shown = true;
-				if (copied) continue;
-				if (!author || c.author === author) cd.shown = true;
+		if (commitData) {
+			for (let cd of commitData) {
+				cd.shown = false;
+				for (let c of cd.commits) {
+					if (c.selected) cd.shown = true;
+					if (copied) continue;
+					if (!author || c.author === author) cd.shown = true;
+				}
 			}
+			apps = commitData.map(cd => cd.repo.replace(/\/$/, ''));
 		}
-		apps = commitData.map(cd => cd.repo.replace(/\/$/, ''));
 	}
 </script>
 
-<h1>Hi{author ? ` ${author}` : ''}!<br/>Here are recent commits.</h1>
-
-{#if copied}
+{#if error}
+<h1>Oh no! Something went wrong. Here's the error you hit:</h1>
+<pre>
+	<code>{error}</code>
+</pre>
 <hr>
-<p><b>Nice!</b> That's been copied. Go paste it into your Notion ticket :)</p>
-{:else if failed}
-<hr>
-<p>Sorry, you need to select some commits first. (click on them)</p>
-{:else}
-<p>Click to select the relevant commits, then Cmd + C to copy.</p>
+<p></p>
+<p></p>
 {/if}
 
-<hr>
+{#if commitData}
 
-{#if !copied}
+	<h1>Hi{ogAuthor ? ` ${ogAuthor}` : ''}!<br/>Here are recent commits.</h1>
 
-<p>Author&nbsp;&nbsp;|&nbsp;&nbsp;
-<button class:is-selected={!author} on:click={() => {author = false}}>All</button>
-{#each authors as a}
-	<button class:is-selected={author === a} on:click={() => {if (author === a) {author = false} else {author = a}; commitData = [...commitData];}}>{a}</button>
-{/each}
-</p>
+	{#if !ogAuthor}
+		<hr>
+		<p style="margin: 3em 0;">Psst - I couldn't figure out who you are.<br/>Make sure you have run <code>git config --global user.name "George Phillips"</code> on your machine so I can filter to your commits.</p>
+		<hr>
+	{/if}
 
-<p>Apps&nbsp;&nbsp;|&nbsp;&nbsp;
-<button class:is-selected={!app} on:click={() => {app = false}}>All</button>
-{#each apps as a}
-	<button class:is-selected={app === a} on:click={() => {if (app === a) {app = false} else {app = a}; commitData = [...commitData];}}>{a}</button>
-{/each}
-</p>
+	{#if copied}
+	<hr>
+	<p><b>Nice!</b> That's been copied. Go paste it into your Notion ticket :)</p>
+	{:else if failed}
+	<hr>
+	<p>Sorry, you need to select some commits first. (click on them)</p>
+	{:else}
+	<p>Click to select the relevant commits, then Cmd + C to copy.</p>
+	{/if}
 
-{/if}
+	<hr>
 
-<div class="c-app" class:is-copied={copied}>
-{#each commitData as cd}
-{#if cd.shown && (!app || app === cd.repo.replace(/\/$/, ''))}
-{#if !copied}
-	<h2>{cd.repo.replace(/\/$/, '')}</h2>
-{/if}
-	<ul class="c-app__commitlist">
-	{#each cd.commits as c}
-		<li class="c-app__commit" 
-			class:is-selected={c.selected} 
-			class:is-author={!author || c.author === author} 
-			on:click={() => {toggle(c)}}>
+	{#if !copied}
 
-			<span class="commitinfo">
-			<kbd class="when">{c.relative_date}</kbd>
-			<bold class="author">{c.author.split(' ')[0]}</bold>
-			</span>
-			<code>{c.message}</code>
-		</li>
+	<p>Author&nbsp;&nbsp;|&nbsp;&nbsp;
+	<button class:is-selected={!author} on:click={() => {author = false}}>All</button>
+	{#each authors as a}
+		<button class:is-selected={author === a} on:click={() => {if (author === a) {author = false} else {author = a}; commitData = [...commitData];}}>{a}</button>
 	{/each}
-	</ul>
-{/if}
-{/each}
-</div>
+	</p>
 
+	<p>Apps&nbsp;&nbsp;|&nbsp;&nbsp;
+	<button class:is-selected={!app} on:click={() => {app = false}}>All</button>
+	{#each apps as a}
+		<button class:is-selected={app === a} on:click={() => {if (app === a) {app = false} else {app = a}; commitData = [...commitData];}}>{a}</button>
+	{/each}
+	</p>
+
+	{/if}
+
+	<div class="c-app" class:is-copied={copied}>
+	{#each commitData as cd}
+	{#if cd.shown && (!app || app === cd.repo.replace(/\/$/, ''))}
+	{#if !copied}
+		<h2>{cd.repo.replace(/\/$/, '')}</h2>
+	{/if}
+		<ul class="c-app__commitlist">
+		{#each cd.commits as c}
+			<li class="c-app__commit" 
+				class:is-selected={c.selected} 
+				class:is-author={!author || c.author === author} 
+				on:click={() => {toggle(c)}}>
+
+				<span class="commitinfo">
+				<kbd class="when">{c.relative_date}</kbd>
+				<bold class="author">{c.author.split(' ')[0]}</bold>
+				</span>
+				<code>{c.message}</code>
+			</li>
+		{/each}
+		</ul>
+	{/if}
+	{/each}
+	</div>
+
+{/if}
